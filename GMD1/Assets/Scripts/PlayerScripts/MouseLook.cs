f051e5bf3bfd56dee4b20201a64908b64a8f72ce
Xbey25 <92146595+Xbey25@users.noreply.github.com>
2024-05-30 11:03:10 +0200

    modified mouselook script

--- GMD1/Assets/Scripts/PlayerScripts/MouseLook.cs
+++ GMD1/Assets/Scripts/PlayerScripts/MouseLook.cs
@@ -4,43 +4,44 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 1.5f;
-    public float smoothing = 2f;
+    // Variables
+    public Transform player;
+    public float mouseSensitivity = 2f;
+    public float bobAmount = 0.1f; // Amount of head bobbing
+    public float bobSpeed = 10f; // Speed of head bobbing
+    private float defaultPosY;
+    float cameraVerticalRotation = 0f;
+    bool lockedCursor = true;
 
-    private float xMousePosition;
-    private float smoothedMousePosition;
-
-    private float currentLookingPosition;
-
-    private void Start()
+    void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        // Lock and Hide the Cursor
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        defaultPosY = transform.localPosition.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        GetInput();
-        ModifyInput();
-        MovePlayer();
-    }
+        // Collect Mouse Input
+        float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-    void GetInput()
-    {
-        xMousePosition = Input.GetAxisRaw("Mouse X");
-    }
+        // Rotate the Camera around its local X axis
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
+        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
 
-    void ModifyInput()
-    {
-        xMousePosition *= mouseSensitivity * smoothing;
-        smoothedMousePosition = Mathf.Lerp(smoothedMousePosition, xMousePosition, 1f / smoothing);
-        currentLookingPosition += smoothedMousePosition;
-    }
+        // Rotate the Player Object around its Y axis
+        player.Rotate(Vector3.up * inputX);
 
-    void MovePlayer()
-    {
-        currentLookingPosition += smoothedMousePosition;
-        transform.localRotation = Quaternion.AngleAxis(currentLookingPosition, transform.up);
+        // Head bobbing effect
+        float movement = Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"));
+        float bobAmountThisFrame = Mathf.Sin(Time.time * bobSpeed) * bobAmount;
+
+        // Apply head bobbing effect
+        Vector3 newPos = transform.localPosition;
+        newPos.y = defaultPosY + bobAmountThisFrame * movement;
+        transform.localPosition = newPos;
     }
 }
