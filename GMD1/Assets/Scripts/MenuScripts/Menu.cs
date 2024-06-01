2bc8e3b807033c1da8e2b04dd7f18c0c5e78cc82
BiancaBadeu <92147004+BiancaBadeu@users.noreply.github.com>
2024-06-01 19:29:46 +0200

    menunav

--- GMD1/Assets/Scripts/MenuScripts/Menu.cs
+++ GMD1/Assets/Scripts/MenuScripts/Menu.cs
@@ -6,7 +6,7 @@ public class Menu : MonoBehaviour
 {
     public GameObject MainMenu;
     public GameObject OptionsMenu;
-        public MenuNav menuNav;
+    public MenuNav menuNav;
 
     public void OnPlayButton ()
     {
