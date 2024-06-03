5c2a40b50dd8fe6e7d482e47d653d9ffc0c0e06f
Xbey25 <92146595+Xbey25@users.noreply.github.com>
2024-06-03 16:53:05 +0200

    jumpscare fixed

--- GMD1/Assets/Scripts/Jumpscare.cs
+++ GMD1/Assets/Scripts/Jumpscare.cs
@@ -8,20 +8,28 @@ public class Jumpscare : MonoBehaviour
     public AudioSource jumpscareSound; 
     public float displayDuration = 1.5f; // Duration to display the jumpscare
 
+
+    private void Start()
+    {
+        jumpscareImage.enabled = false;
+        jumpscareSound.enabled = false;
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Player entered the trigger area.");
             StartCoroutine(DisplayJumpscare());
+            jumpscareImage.enabled = true;
+            jumpscareSound.enabled = true;
         }
     }
 
     private IEnumerator DisplayJumpscare()
     {
         Debug.Log("Jumpscare started.");
-        jumpscareImage.enabled = true;
-        jumpscareSound.enabled = true;
         jumpscareSound.Play();
 
         yield return new WaitForSeconds(displayDuration);
